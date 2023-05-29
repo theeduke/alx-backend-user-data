d5408c3be995c6b907b8b0d156de9c1b2e5e942c
theeduke <oscarian528@gmail.com>
2023-05-29 15:29:41 -0700

    update

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -7,12 +7,21 @@ from api.v1.views import app_views
 
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def authorized() -> str:
-    """Get /api/v1/status
-    
+    """ GET /api/v1/unauthorized
     Return:
         - raise a 401 error
     """
     abort(401, description="Unauthorized")
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbid() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+        - raise a 403 error
+    """
+    abort(403, description="Forbidden")
+
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
